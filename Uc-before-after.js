f9cc4e31fe5d963a3ad237892faf608a089d44d1
Mike-AaronrichMarketing <109161216+Mike-AaronrichMarketing@users.noreply.github.com>
2023-11-22 11:06:46 -0600

    Create Uc-before-after.js
    This adds a if else statement to look for the picture tag this applied by Imagify on the site.

---
+++ Uc-before-after.js
@@ -0,0 +1,26 @@
+ key: "createElements",
+                                        value: function () {
+                                            var e = document.createElement("span");
+                                            (e.className = this.options.dragElementSelector.replace(".", "")), this.element.appendChild(e);
+                                            
+                                            //Modification to work with imagify. The if else statement will check for img or picture tag in order to use the before after.
+                                            if ( n =="img:first-child"){                                    
+                                                var t = document.createElement("div"),
+                                                n = this.element.querySelector("img:first-child");
+                                                t.appendChild(n.cloneNode(!0)),
+                                                n.parentNode.replaceChild(t, n),
+                                                (this.dragElement = this.element.querySelector(this.options.dragElementSelector)),
+                                                (this.beforeElement = this.element.querySelector("div:first-child")),
+                                                (this.beforeImage = this.beforeElement.querySelector("img"));
+                                            }
+                                            else {
+                                                var t = document.createElement("div"),
+                                                n = this.element.querySelector("picture:first-child");
+                                                t.appendChild(n.cloneNode(!0)),
+                                                n.parentNode.replaceChild(t, n),
+                                                (this.dragElement = this.element.querySelector(this.options.dragElementSelector)),
+                                                (this.beforeElement = this.element.querySelector("div:first-child")),
+                                                (this.beforeImage = this.beforeElement.querySelector("img"));
+                                            }    
+                                        },
+                                    },
